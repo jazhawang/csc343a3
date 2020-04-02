684004ca870857fe5a5480f740bba1471c030621
unknown <jinggg3@gmail.com>
2020-04-02 19:17:40 -0400

    updating q2 to contain the query, q4 dummy file

--- q2.sql
+++ q2.sql
@@ -1,13 +1,10 @@
--- Q2. Refunds!
-
 -- You must not change the next 2 lines or the table definition.
 SET SEARCH_PATH TO wetworldschema;
 DROP TABLE IF EXISTS q2 CASCADE;
 
 CREATE TABLE q2 (
-    siteID INT NOT NULL,
 		monitorID INT NOT NULL,
-		maxRating INT NOT NULL
+
 );
 
 -- Do this for each of the views that define your intermediate steps.
@@ -18,33 +15,56 @@ CREATE TABLE q2 (
 -- Get the average rating for each monitor at each location (and their corre
 -- -sponding information such as email address)
 
-DROP VIEW IF EXISTS avgRatings CASCADE;
-CREATE VIEW avgRatings AS
-	select monitorID, avg(monitorRating) as avgRating, emailAddress
+DROP VIEW IF EXISTS avgmRatings CASCADE;
+CREATE VIEW avgmRatings AS
+	select monitorID, avg(monitorRating) as avgRating
 	from booking
-	group by monitorID, emailAddress;
-
--- Get the average prices that each monitor charges across all divesites and
--- divetypes
-
-DROP VIEW IF EXISTS avgMonitorPrice CASCADE;
-CREATE VIEW avgMonitorPrice AS
-	select monitorID, avg(pricing) as avgPrice
-	from MonitorPricing
 	group by monitorID;
 
--- Find the highest rated monitor for each location
-DROP VIEW IF EXISTS maxRating CASCADE;
-CREATE VIEW maxRating AS
-	select monitorID, max(avgRating), siteID, emailAddress
-	from avgRatings
-	group by monitorID, siteID, emailAddress;
+-- Get the average rating at each location
 
-DROP VIEW IF EXISTS maxRatingMonitorPrice CASCADE;
-CREATE VIEW maxRatingMonitorPrice AS
-	select m.monitorID as mID, m.emailAddress as email, a.avgPrice as price
-	from maxRating as m, avgMonitorPrice as a
-	where m.monitorID = a.monitorID
+DROP VIEW IF EXISTS avgsRatings CASCADE;
+CREATE VIEW avgsRatings AS
+	select siteID, avg(monitorRating) as avgRating
+	from booking
+	group by siteID;
+
+-- Get all the locations that a monitor dives at
+
+DROP VIEW IF EXISTS monitorAllLocations CASCADE;
+CREATE VIEW monitorAllLocations AS
+	select monitorID, diveSite
+	from MonitorPricing;
+
+-- Combine the average ratings of each monitor with the locations
+-- and the ratings of those locations
+
+DROP VIEW IF EXISTS monitorSiteRatings CASCADE;
+CREATE VIEW monitorSiteRatings AS
+	select m.monitorID as mID, m.avgRating as avgMonitorRating,
+		s.avgRating as avgSiteRating
+	from avgmRatings as m, avgsRatings as s, monitorAllLocations as a
+	where m.monitorID = a.monitorID and a.diveSite = s.siteID;
+
+-- Filter out the monitors that have at least a lower average rating than a
+-- specific location
+
+DROP VIEW IF EXISTS badMonitors CASCADE;
+CREATE VIEW badMonitors AS
+	select distinct m.monitorID
+	from monitorSiteRatings as m
+	where m.avgMonitorRating < m.avgSiteRating;
+
+-- Find the monitors that are on average better than all their locations
+
+DROP VIEW IF EXISTS goodMonitors CASCADE;
+CREATE VIEW goodMonitors AS
+	select distinct m.monitorID
+	from monitorSiteRatings as m
+	where NOT EXISTS badMonitors;
+
+-- Find avg booking fee and email addresses accordingly
+
 
 
 -- Your query that answers the question goes below the "insert into" line:
